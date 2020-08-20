e11db8421acf87c7b440b13f7522d7afef8ae281
Leon van Ginneken <leon.van.ginneken@alliander.com>
2020-08-20 22:37:37 +0200

    werk parkeren

--- scripts/compile-tsc.js
+++ scripts/compile-tsc.js
@@ -6,7 +6,7 @@ const shell = require('shelljs');
 function getCommand(watch) {
   const tsc = path.join(__dirname, '..', 'node_modules', '.bin', 'tsc');
 
-  const args = ['--outDir ./app/ui5/dist', '--listEmittedFiles true'];
+  const args = ['--outDir ./dist', '--listEmittedFiles true'];
 
   /**
    * Only emit declarations if it does not need to be compiled with tsc
