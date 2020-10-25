1c18ea909f092af9d26bd007816cd85b3ec1b3be
Leon <leonvanginneken@gmail.com>
2020-10-25 07:08:41 +0100

    make inline rendering work in example

--- examples/ui5-kitchen-sink/.storybook/preview.js
+++ examples/ui5-kitchen-sink/.storybook/preview.js
@@ -1,5 +1,30 @@
+const react = require("react");
 export const parameters = {
     docs: {
-      inlineStories: false
+      inlineStories: true,
+      prepareForInline: function prepareForInline(storyFn) {
+        var element = storyFn();
+        const html = document.createElement("div");
+
+        const controller = element.controller ? element.controller : {};
+
+        sap.ui.getCore().attachInit(() => {
+          sap.ui.require(["sap/ui/core/mvc/XMLView", "sap/ui/core/mvc/Controller"], async function(XMLView, ControllerClass) {
+            const Controller = ControllerClass.extend("whatever", controller)
+            const fragment = await XMLView.create({
+              definition: `<mvc:View xmlns:mvc="sap.ui.core.mvc">${element.template ? element.template : element}</mvc:View>`,
+              controller: controller ? new Controller(): undefined
+            });
+            fragment.placeAt(html, "only");
+          });
+        });
+  
+        return react.createElement("div", {
+          ref: function ref(node) {
+            return node ? node.appendChild(html) : null;
+          }
+        });
+
+      }
 	}
 }
